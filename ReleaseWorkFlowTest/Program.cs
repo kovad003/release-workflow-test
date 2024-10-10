faef5dec1d0a2c18a73004f6034c7cfa362a0e79
kovad003 <kovacsdansan@gmail.com>
2024-10-10 23:02:49 +0200

    [2571] changed the action of the sign in button

--- ReleaseWorkFlowTest/Program.cs
+++ ReleaseWorkFlowTest/Program.cs
@@ -3,6 +3,6 @@ Console.WriteLine("Hello, World!");
 
 
 Console.WriteLine(@"<btn> Log in button <\btn>");
-Console.WriteLine(@"<btn> Sign in button <\btn>");
+Console.WriteLine(@"<btn action='Post'> Sign in button <\btn>");
 
 
